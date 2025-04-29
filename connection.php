55acaded9f80d2c09b0225bf5ff0955b2bcf0a46
marc1j2b <marc.braunagel@controlware.de>
2025-04-29 09:38:36 +0200

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,17 @@
 <?php
 
-    $dsn = 'mysql:dbname=userdb;host=localhost';
-    $username = 'root';
-    $password = '';
+$dsn = 'mysql:dbname=userdb;host=localhost';
+$username = 'root';
+$password = '';
 
+try {
     $con = new PDO($dsn, $username, $password);
+    // Fehler als Ausnahmen behandeln
+    $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Fehler ausgeben (im Produktivsystem besser: Fehler protokollieren und nicht direkt ausgeben)
+    echo 'Connection failed: ' . $e->getMessage();
+    exit;
+}
+
 ?>

40a6ab033ec12beae2a19e9fdcb99214aca90239
marc1j2b <marc.braunagel@controlware.de>
2025-04-29 09:39:51 +0200

    Update details.trikot.php

--- details.trikot.php
+++ details.trikot.php
@@ -1,29 +1,30 @@
 <?php
+// Verbindung zur Datenbank mit Fehlerprüfung
+$con = mysqli_connect('localhost', 'root', '', 'webshop');
+if (!$con) {
+    die("Verbindung zur Datenbank fehlgeschlagen: " . mysqli_connect_error());
+}
 
-$con = mysqli_connect('localhost', 'root');
-mysqli_select_db($con, 'webshop');
-$sql = "SELECT * FROM products WHERE featured=1";
-$featured = $con->query($sql);
-
+// SQL-Abfrage ausführen
+$sql = "SELECT * FROM products WHERE featured = 1";
+$featured = mysqli_query($con, $sql);
 ?>
+
 
 <div class="col-md-2"></div>
 
 <div class="pro-container">
-            <div class="des">
-                <h2 class="text-center">Product Details</h2>
-                <?php
-                     while($product = mysqli_fetch_assoc($featured)):
+    <div class="des">
+        <h2 class="text-center">Product Details</h2>
 
-
-                ?>
-                <div class="pro">
-                    <h4><b> <?= $product['title'];?></b></h4>
-                    <img src="<?= $product['image'];?>" alt="<?= $product['title']; ?>"/>
-                    <p class="lprice"><b>$ <?= $product['price'];?></b></p>
-                    <p class="desc"><b><?= $product['description'];?></b></p>
-                    <p class="bname"><b><?= $product['brandname'];?></b></p>
-                </div>
-                <?php endwhile; ?>
+        <?php while ($product = mysqli_fetch_assoc($featured)): ?>
+            <div class="pro">
+                <h4><b><?= htmlspecialchars($product['title']); ?></b></h4>
+                <img src="<?= htmlspecialchars($product['image']); ?>" alt="<?= htmlspecialchars($product['title']); ?>" />
+                <p class="lprice"><b>$<?= htmlspecialchars($product['price']); ?></b></p>
+                <p class="desc"><b><?= htmlspecialchars($product['description']); ?></b></p>
+                <p class="bname"><b><?= htmlspecialchars($product['brandname']); ?></b></p>
             </div>
-        </div>
+        <?php endwhile; ?>
+    </div>
+</div>
